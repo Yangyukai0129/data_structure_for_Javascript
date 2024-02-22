9258347c0315d140cc08c7458e5701683a4c5d71
kevin_yang <kevin_yang@comwave.com.tw>
2024-02-22 10:47:18 +0800

    add shift()

--- Singly_Linked_List.js
+++ Singly_Linked_List.js
@@ -40,6 +40,16 @@ class SingleLinedList {
         }
         return current;
     }
+    shift() {
+        if (!this.head) return undefined;
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return currentHead;
+    }
 }
 
 let list = new SingleLinedList()
