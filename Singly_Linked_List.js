16e9352cf0ada348ba7e66a83715182bb04baa85
kevin_yang <kevin_yang@comwave.com.tw>
2024-02-22 10:28:01 +0800

    add pop()

--- Singly_Linked_List.js
+++ Singly_Linked_List.js
@@ -23,9 +23,28 @@ class SingleLinedList {
         this.length++;
         return this;
     }
+    pop() {
+        if (!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
 }
 
 let list = new SingleLinedList()
 list.push("hello");
+list.push("goodbye");
+list.push("!");
 // list.push("goodbye");
 console.log(list);
