58f32c5316e73af1caa8ebe6963d160cdb9eb25d
kevin_yang <kevin_yang@comwave.com.tw>
2024-02-26 18:09:16 +0800

    first commit

--- Binary_Search_Tree.js
+++ Binary_Search_Tree.js
@@ -49,6 +49,34 @@ class BinarySearchTree {
         }
         return current;
     }
+    contains(value) {
+        if (this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+    BFS() {
+        let node = this.root;
+        let data = [];
+        let queue = [];
+        queue.push(node);
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
 }
 
 let tree = new BinarySearchTree();
