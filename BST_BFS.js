6b70e07978982600a07a5c07c807f803320e8ce4
Yangyukai0129 <beir6108@gmail.com>
2024-03-01 21:24:14 +0800

    first commit

---
+++ BST_BFS.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    BFS() {
+        let node = this.root;
+        let data = [];
+        let queue = [];
+        queue.push(node);
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
